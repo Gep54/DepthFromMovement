25991f59ae28c9f101cc083af3f908ff2b0e132f
vberan <vaclav.beran@creativedock.com>
2026-05-19 01:51:49 +0200

    camera axis swap gone

--- ros2_ws/src/incremental_vo_ros2/incremental_vo_ros2/param_config.py
+++ ros2_ws/src/incremental_vo_ros2/incremental_vo_ros2/param_config.py
@@ -56,7 +56,6 @@ KNOWN_PARAMETERS = frozenset(
         "offline_dataset_image_prefix",
         "base_frame",
         "camera_frame",
-        "camera_axes_swap",
         "tf_lookup_period_s",
         "tf_use_latest_time",
         "tf_static_volatile_qos",
